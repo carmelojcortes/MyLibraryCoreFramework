9ee114eb1ed0f1819e6b39ef42612ccf739b38e5
Carmelo J Cortés Alhambra <>
2023-06-22 14:01:38 +0200

    changed names, seems conflicting with the name of the xcframework module

--- Package.swift
+++ Package.swift
@@ -6,14 +6,14 @@ let package = Package(
     platforms: [.iOS(.v14)],
     products: [
         .library(
-            name: "MyLibCore",
-            targets: ["MyLibCore"]),
+            name: "MyLibraryCore",
+            targets: ["MyLibraryCore"]),
     ],
     dependencies: [
     ],
     targets: [
         .binaryTarget(
-            name: "MyLibCore",
+            name: "MyLibraryCore",
             url: "https://github.com/carmelojcortes/MyLibrary/releases/download/release%2Ftest13/MyLibCore.zip",
             checksum: "1ea147b0072b8e68ff11f044612bf59f8377949aed33d74f7ae32dcb93de73d4"
         )
